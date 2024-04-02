fc85ef3795ae36f57dd31f417e452a5f5faa3914
Bhangale, Ketan <kbhanga1@wm.com>
2024-04-02 16:44:37 +0530

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+c
+3app/src/main/java/com/sonarqubedemo/MainActivity.kt,2\6\2614acb92d6fb7669c0deb7b42fbf810dced8dc8
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+e
+5app/src/main/java/com/sonarqubedemo/ui/theme/Color.kt,5\d\5dcdf44ca5c24e5bec9a99901dfea5866c84dd52
+>
+app/.gitignore,5\1\51e1c5d383dfaa35e0e7e5873a0a99355a86880f
---
+++ app/src/main/java/com/sonarqubedemo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.sonarqubedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import java.io.File
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        calculateNumber()
+        writeToFile()
+    }
+
+    private fun writeToFile() {
+        File(applicationContext.filesDir, "test.txt").printWriter().use { out ->
+            //out.println("${it.key}, ${it.value}")
+        }
+    }
+
+    private fun calculateNumber() {
+        var i = 25
+        var no = i/0
+
+
+    }
+    public fun emptyMe(){
+
+    }
+
+}
+
